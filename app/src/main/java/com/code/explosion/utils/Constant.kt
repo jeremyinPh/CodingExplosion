b332f98ff3b965d5a70946ffc44a912e89814f3a
Jeremy <Jeremy@leegame.net>
2020-01-11 19:08:59 +0800

    init db --> room

--- app/src/main/java/com/code/explosion/utils/Constant.kt
+++ app/src/main/java/com/code/explosion/utils/Constant.kt
@@ -3,4 +3,5 @@ package com.code.explosion.utils
 // for router
 const val PATH_MAIN: String = "/level_one/main"
 
-
+const val DATABASE_NAME = "sunflower-db"
+const val PLANT_DATA_FILENAME = "plants.json"
