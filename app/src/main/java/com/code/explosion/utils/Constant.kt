af97505ab9e527a92ff78574e5f972c761df9ef9
Jeremy <Jeremy@leegame.net>
2020-01-15 13:23:50 +0800

    single instance

--- app/src/main/java/com/code/explosion/utils/Constant.kt
+++ app/src/main/java/com/code/explosion/utils/Constant.kt
@@ -1,7 +1,13 @@
 package com.code.explosion.utils
 
-// for router
-const val PATH_MAIN: String = "/level_one/main"
+// 单例
+object Constant {
+    // for router
+    const val PATH_MAIN: String = "/level_one/main"
 
-const val DATABASE_NAME = "sunflower-db"
-const val PLANT_DATA_FILENAME = "plants.json"
+    const val DATABASE_NAME = "sunflower-db"
+    const val PLANT_DATA_FILENAME = "plants.json"
+
+
+}
+
